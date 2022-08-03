99e251c3ba9682381a8bfebc60ad070aa98a821a
FrasMaciej <maciejfrs@gmail.com>
2022-08-03 17:10:40 +0200

    little improvement of doctors list

--- medApp/src/app/admin-page/admin-page.component.ts
+++ medApp/src/app/admin-page/admin-page.component.ts
@@ -8,15 +8,21 @@ import { DoctorService } from '../services/doctor.service';
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent implements OnInit {
-  doctors: any;
-  
+  doctors: Doctor[] | any;
+
   constructor(private doctorService: DoctorService) { } 
 
   ngOnInit(): void {
     this.doctorService.getDoctors().subscribe(data => {
       console.log(data);
       this.doctors = data;
+      this.doctors.sort((a:any,b:any) => (a.surname < b.surname ? -1 : 1));
     })
+  }
+
+  onRemove(e: Event) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
   }
 
 }
