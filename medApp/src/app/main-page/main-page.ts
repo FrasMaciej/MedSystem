d16309be4d9c7375f4320694b27787b8c9654600
FrasMaciej <maciejfrs@gmail.com>
2022-08-19 15:29:45 +0200

    refactore

--- medApp/src/app/main-page/main-page.ts
+++ medApp/src/app/main-page/main-page.ts
@@ -46,6 +46,10 @@ import { Router } from '@angular/router';
       flex: auto;
     }
 
+    .hover-class:hover {
+      background-color: rgb(40, 123, 134);
+      color: white;
+    }
   `]
 })
 export class MainPageComponent implements OnInit {
