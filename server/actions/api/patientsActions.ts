bbcc5501259737e34ed0cd45d91530bf3f70c296
FrasMaciej <maciejfrs@gmail.com>
2022-08-22 16:02:20 +0200

    raw register endpoint added (needs to add errors check)

--- server/actions/api/patientsActions.ts
+++ server/actions/api/patientsActions.ts
@@ -1,10 +1,17 @@
-export const Patient = require('../../db/models/patient');
+// export const Patient = require('../../db/models/patient');
+const Patient = require('../../db/models/patient');
 import { Request, Response } from 'express';
 
 
 class PatientActions {
     login(req: Request, res: Response) {
         res.redirect('/api/doctors');
+    }
+
+    register(req: Request, res: Response) {
+        const username: String = req.body.username;
+        const password: String = req.body.password;
+        Patient.register({username: username, active: false}, password);
     }
 }
 
